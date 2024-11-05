a50068b6e5242c8e2e27bdfbeeea3a74f8632e57
Yasmina Maddah <maddahyasmina@gmail.com>
2024-11-05 23:35:04 +0200

    Added Functionality to the Scroll Button

--- Scripts/index.js
+++ Scripts/index.js
@@ -22,4 +22,19 @@ const learnMoreBtn = document.getElementById('learnMoreBtn');
         document.body.removeChild(contactPopup);
       });
 });
+
+const scrollTopBtn = document.getElementById("scrollTopBtn");
+  window.onscroll = function () { toggleScrollTopBtn() };
+  
+  function toggleScrollTopBtn() {
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+      scrollTopBtn.style.display = "block";
+    } else {
+      scrollTopBtn.style.display = "none";
+    }
+  }
+  
+  scrollTopBtn.addEventListener('click', () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
   
